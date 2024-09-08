5834200ec0f14fdc1ac4763a89934cc5e0f8d404
Robin Wragg <robin.wragg@me.com>
2024-09-08 13:41:02 +0100

    Solids!

--- src/game.rs
+++ src/game.rs
@@ -1,23 +1,18 @@
-use crate::grid;
+use crate::grid::*;
 use crate::prelude::*;
 
 pub struct Game {
     debugger: Debugger,
     launch_time: Instant,
     prev_frame_start_time: Instant,
-    grid: Vec<Vec<f32>>,
+    grid: Vec<Vec<Atom>>,
     pub events: EventMgr,
     transform: Mat4,
 }
 
 impl Game {
     pub fn new(aspect_ratio: f32) -> Game {
-        let mut grid = vec![vec![0.0f32; grid::GRID_SIZE]; grid::GRID_SIZE];
-        for column in grid.iter_mut() {
-            for y in column {
-                *y = 0.0;
-            }
-        }
+        let mut grid = vec![vec![Atom::Gas(0.0); GRID_SIZE]; GRID_SIZE];
 
         let transform = Mat4::from_translation(Vec3::new(-0.9, -0.9, 0.0))
             * Mat4::from_scale(Vec3::new(0.05 / aspect_ratio, 0.05, 1.0));
@@ -38,7 +33,7 @@ impl Game {
                 let v = transform_2d(&pos, &self.transform.inverse());
                 let x = v.x as usize;
                 let y = v.y as usize;
-                self.grid[x][y] = 5000.0;
+                self.grid[x][y] = Atom::Gas(5000.0);
                 true
             }
             _ => false,
@@ -57,15 +52,14 @@ impl Game {
 
         let mesh = Mesh::new(&verts, None, None, gpu);
 
-        for x in 0..grid::GRID_SIZE {
-            for y in 0..grid::GRID_SIZE {
-                let v = self.grid[x][y] * 0.01;
+        for x in 0..GRID_SIZE {
+            for y in 0..GRID_SIZE {
+                let color = match self.grid[x][y] {
+                    Atom::Gas(v) => Vec4::new(v * 0.01, 0.0, 1.0 - v * 0.01, 1.0),
+                    Atom::Solid => Vec4::new(0.0, 1.0, 0.0, 1.0),
+                };
                 let m = Mat4::from_translation(Vec3::new(x as f32, y as f32, 0.0));
-                gpu.render_mesh(
-                    &mesh,
-                    &(self.transform * m),
-                    Some(Vec4::new(v, 0.0, 1.0 - v, 1.0)),
-                );
+                gpu.render_mesh(&mesh, &(self.transform * m), Some(color));
             }
         }
     }
@@ -86,7 +80,7 @@ impl Game {
             }
         }
 
-        grid::update_with_2x2_equilibrium(&mut self.grid);
+        update_with_2x2_equilibrium(&mut self.grid);
         self.render_grid(gpu);
 
         // std::thread::sleep(std::time::Duration::from_millis(500)); // TODO
--- src/grid.rs
+++ src/grid.rs
@@ -1,18 +1,52 @@
 use crate::prelude::*;
 
-pub const GRID_SIZE: usize = 32; // TODO 64
+pub const GRID_SIZE: usize = 64; // TODO 64
 
-// This also works with i32, but the /4 causes small losses. Not a problem in a vaccuum where we expect losses, but doesn't model an airtight space well.
-pub fn update_with_2x2_equilibrium(grid: &mut Vec<Vec<f32>>) {
+#[derive(Copy, Clone)]
+pub enum Atom {
+    Gas(f32),
+    Solid,
+}
+
+fn mut_gas_pressures(grid: &mut Vec<Vec<Atom>>, x: usize, y: usize) -> Vec<&mut f32> {
+    let mut pressures = vec![];
+
+    let (column_a, column_b) = grid.split_at_mut(x + 1);
+    let (cell_a, cell_b) = column_a[x].split_at_mut(y + 1);
+    let (cell_c, cell_d) = column_b[0].split_at_mut(y + 1);
+
+    if let Atom::Gas(pressure) = &mut cell_a[y] {
+        pressures.push(pressure);
+    }
+    if let Atom::Gas(pressure) = &mut cell_b[0] {
+        pressures.push(pressure);
+    }
+    if let Atom::Gas(pressure) = &mut cell_c[y] {
+        pressures.push(pressure);
+    }
+    if let Atom::Gas(pressure) = &mut cell_d[0] {
+        pressures.push(pressure);
+    }
+
+    pressures
+}
+
+pub fn update_with_2x2_equilibrium(grid: &mut Vec<Vec<Atom>>) {
     debug_assert!(GRID_SIZE % 2 == 0);
 
-    fn reach_local_equilibrium(grid: &mut Vec<Vec<f32>>, x: usize, y: usize) {
-        let total = grid[x][y] + grid[x + 1][y] + grid[x][y + 1] + grid[x + 1][y + 1];
-        let total_over_4 = total / 4.0;
-        grid[x][y] = total_over_4;
-        grid[x + 1][y] = total_over_4;
-        grid[x][y + 1] = total_over_4;
-        grid[x + 1][y + 1] = total_over_4;
+    fn reach_local_equilibrium(grid: &mut Vec<Vec<Atom>>, x: usize, y: usize) {
+        let pressures = mut_gas_pressures(grid, x, y);
+
+        let mut pressure_total = 0.0;
+        for pressure in &pressures {
+            pressure_total += **pressure;
+        }
+
+        let total_over_4 = pressure_total / 4.0;
+
+        for pressure in pressures {
+            *pressure = total_over_4;
+        }
     }
 
     for x in (0..GRID_SIZE).step_by(2) {
@@ -25,66 +59,5 @@ pub fn update_with_2x2_equilibrium(grid: &mut Vec<Vec<f32>>) {
         for y in (1..GRID_SIZE - 1).step_by(2) {
             reach_local_equilibrium(grid, x, y);
         }
-    }
-}
-
-fn update_with_shuffle23(grid: &mut Vec<Vec<f32>>) {
-    fn iterate(grid: &mut Vec<Vec<f32>>, iteration: usize) {
-        let mut changes = vec![vec![0.0; GRID_SIZE]; GRID_SIZE];
-
-        let directions = vec![
-            (1i32, 0i32),
-            (1, 1),
-            (0, 1),
-            (-1, 1),
-            (-1, 0),
-            (-1, -1),
-            (0, -1),
-            (1, -1),
-        ];
-
-        let inv_sqrt2 = 1.0 / SQRT_2;
-        let weights = vec![
-            1.0, inv_sqrt2, 1.0, inv_sqrt2, 1.0, inv_sqrt2, 1.0, inv_sqrt2,
-        ];
-
-        let direction = directions[iteration];
-        let weight = weights[iteration];
-
-        for x in 1..grid.len() - 1 {
-            for y in 1..grid[x].len() - 1 {
-                if grid[x][y] < weight {
-                    // TODO: set to 0?
-                    continue;
-                }
-
-                let x32 = x as i32;
-                let y32 = y as i32;
-                if grid[(x32 + direction.0) as usize][(y32 + direction.1) as usize] < grid[x][y] {
-                    // println!(
-                    //     "transferring from {} {} to {} {}",
-                    //     x,
-                    //     y,
-                    //     x32 + direction.0,
-                    //     y32 + direction.1,
-                    // );
-                    changes[x][y] -= weight;
-                    changes[(x32 + direction.0) as usize][(y32 + direction.1) as usize] += weight;
-                }
-            }
-        }
-
-        for x in 1..grid.len() - 1 {
-            for y in 1..grid[x].len() - 1 {
-                grid[x][y] += changes[x][y];
-                grid[x][y] = grid[x][y].clamp(0.0, 100000.0);
-            }
-        }
-    }
-
-    let mut seeds = vec![0, 1, 2, 3, 4, 5, 6, 7];
-    seeds.shuffle(&mut thread_rng());
-    for i in &seeds {
-        iterate(grid, *i);
     }
 }
